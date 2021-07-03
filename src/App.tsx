7ef358a7d6667d098f138c56a925f6f430ff3d8e
Meruem117 <1572677319@qq.com>
2021-07-03 17:36:27 +0800

    add proxy

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 import { Button } from 'antd';
 import './App.css';
 
 class App extends Component {
+
+  getData = async (): Promise<void> => {
+    const response = await axios.get(`/api/getHome`)
+    console.log(response.data)
+    // axios.get(`/api/getHome`).then(
+    //   response => { console.log(response.data) },
+    //   error => { console.error(error) }
+    // )
+  }
+
   render() {
     return (
       <div className="flex">
         <p className="text-4xl text-red-500">Hello</p>
-        <Button type="primary">World</Button>
+        <Button type="primary" onClick={this.getData}>World</Button>
       </div>
     )
   }
---
+++ src/setupProxy.js
@@ -0,0 +1,20 @@
+const proxy = require('http-proxy-middleware')
+
+module.exports = function (app) {
+    app.use(
+        proxy('/api', {
+            target: 'http://localhost:5000',
+            changeOrigin: true,
+            pathRewrite: {
+                '^/api': ''
+            }
+        }),
+        // proxy('/api', {
+        //     target: 'http://localhost:5000',
+        //     changeOrigin: true,
+        //     pathRewrite: {
+        //         '^/api': ''
+        //     }
+        // })
+    )
+}
