9d78a62eb6dce1875b1c732169137bdde8325fd9
Meruem117 <1572677319@qq.com>
2021-10-31 23:00:27 +0800

    update

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,8 +1,8 @@
 import React, { Component, lazy, Suspense } from 'react'
 import { Route, Switch, Redirect } from 'react-router-dom'
+import { Spin } from 'antd'
 import Nav from './Nav'
 import Footer from './Footer'
-import { Spin } from 'antd'
 
 const Home = lazy(() => import('./Home'))
 const About = lazy(() => import('./About'))
---
+++ src/constant/index.ts
@@ -0,0 +1,2 @@
+// up
+export const UP_LIST_SIZE = 10
