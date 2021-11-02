31aa409e7c5f1f214bcaa3d2960587fad722cdee
Meruem117 <1572677319@qq.com>
2021-11-02 22:52:09 +0800

    update config

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,11 @@
-// craco.config.js
+const path = require('path')
+
 module.exports = {
+    webpack: {
+        alias: {
+            '@': path.join(__dirname, 'src'),
+        },
+    },
     style: {
         postcss: {
             plugins: [
