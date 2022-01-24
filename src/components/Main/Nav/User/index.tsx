8cdb98d73c5d937d61bd31025ca3040e786ccd0f
Meruem117 <1572677319@qq.com>
2022-01-24 19:03:06 +0800

    use about to use redux

--- src/components/Main/Nav/User/index.tsx
+++ src/components/Main/Nav/User/index.tsx
@@ -1,22 +1,8 @@
 import React from 'react'
-import { Avatar, Button, Popover } from 'antd'
+import { Avatar, Popover } from 'antd'
 import { UserOutlined } from '@ant-design/icons'
-import store from '@/redux/store'
-import { createAddAction, createMinusAction, createAddAsyncAction } from '@/redux/actions/base-action'
 
 const User: React.FC = () => {
-  const clickAdd = () => {
-    store.dispatch(createAddAction())
-  }
-
-  const clickMinus = () => {
-    store.dispatch(createMinusAction())
-  }
-
-  const clickAsync = () => {
-    store.dispatch(createAddAsyncAction(1500))
-  }
-
   const content = (
     <div>
       <p>Content</p>
@@ -25,10 +11,6 @@ const User: React.FC = () => {
 
   return (
     <div className='flex px-4'>
-      <div>{store.getState()}</div>
-      <Button type='primary' onClick={clickAdd}>Add</Button>
-      <Button type='primary' onClick={clickMinus}>Minus</Button>
-      <Button type='primary' onClick={clickAsync}>Async</Button>
       <Popover placement='topLeft' content={content} title='Title'>
         <Avatar className='cursor-pointer' size="large" icon={<UserOutlined />} />
       </Popover>
---
+++ src/containers/About/index.tsx
@@ -0,0 +1,7 @@
+import About from "@/components/Main/About"
+// import store from "@/redux/store"
+import { connect } from 'react-redux'
+
+const AboutContainer = connect()(About)
+
+export default AboutContainer
